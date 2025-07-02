3cf9f64239825a675abbb19eda234651c17cdc6d
RichardHayes <rhshadowgreen@gmail.com>
2025-07-02 16:57:47 -0700

    api

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
-export function main() {
-    console.log("hello");
+import { Octokit } from "https://esm.sh/octokit";
+
+function makeDiv(content, onClick) {
+    const div = document.createElement("div");
+    div.className = "gist";
+    div.innerHTML = content;
+    div.addEventListener("click", onClick);
+    return div;
+}
+
+export async function main({ list, editor }) {
+
+    const octokit = new Octokit({
+        auth: 'TOKEN'
+    });
+
+    const gists = await octokit.request('GET /gists', {
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+
+    for (const { files } of gists.data) {
+        for (const key in files) {
+            const { filename, raw_url } = files[key];
+            
+            list.appendChild(makeDiv(filename, async e => {
+                const response = await fetch(raw_url);
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+                const content = await response.text();
+                editor.innerHTML = content;
+            }));
+
+            break; // Only get first file in Gist
+        }    
+    }
+
+    /* 
+    // Create Gist 
+    await octokit.request('POST /gists', {
+        description: 'Example of a gist',
+        'public': true,
+        files: {
+            'README.md': {
+                content: 'Hello World'
+            }
+        },
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    }); */
 }
